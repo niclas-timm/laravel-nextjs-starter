1dfc328f8359f234dd9fec4da72bd171819efd12
Niclas <niclastimmdev@gmail.com>
2021-01-04 14:53:00 +0100

    Add some markup to index page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,3 +1,35 @@
+/*
+|--------------------------------------------------------------------------
+| The home page.
+|--------------------------------------------------------------------------
+|
+| The home page of your application.
+|
+*/
+import Link from "next/link";
+import { H1 } from "./../components/Typography/Headers";
+
 export default function Home() {
-    return <div>Hello World!</div>;
+    return (
+        <>
+            <div className="flex w-screen h-screen items-center justify-center">
+                <div className="w-1/2">
+                    <H1 center={true} withMargin={true}>
+                        Welcome to the Laravel Next.js Starter Kit
+                    </H1>
+
+                    <p className="text-center mb-4 mt-12">
+                        What do you want to do?
+                    </p>
+                    <div className="flex justify-between items-center text-blue-500 underline">
+                        <Link href="https://github.com/NiclasTimmeDev/laravel-nextjs-starter">
+                            <a target="_blank">Documentation</a>
+                        </Link>
+                        <Link href="/user/login">Login</Link>
+                        <Link href="/user/register">Register</Link>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
